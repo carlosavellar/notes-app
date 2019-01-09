634bccf28035dbae147fcee4b952ca0340a6fe8c
José Carlos <carlos.avellar@gmail.com>
2019-01-09 09:20:49 -0200

    Comments

--- notes-app.js
+++ notes-app.js
@@ -1,32 +1,39 @@
-var uniqueId = function () {
-    return 'id-' + Math.random().toString(36).substr(2, 16);
+var uniqueId = function() {
+  return (
+    "id-" +
+    Math.random()
+      .toString(36)
+      .substr(2, 16)
+  );
 };
 
+// bind localStorage saved notes to the obj Notes
 let notes = getSavedNotes();
 
 const filters = {
-    serachText: ''
+  serachText: ""
 };
 
 renderNotes(notes, filters);
 
-document.querySelector("#create-note").addEventListener('submit', e => {
-    const id = uniqueId();
-    e.preventDefault();
-    notes.push({
-        id,
-        title: e.target.elements.newNote.value,
-        body: e.target.elements.textBody.value
-    });
+// event adding news note
+document.querySelector("#create-note").addEventListener("submit", e => {
+  const id = uniqueId();
+  e.preventDefault();
+  notes.push({
+    id,
+    title: e.target.elements.newNote.value,
+    body: e.target.elements.textBody.value
+  });
 
-    saveNotes(notes);
-    renderNotes(notes, filters);
+  saveNotes(notes);
+  renderNotes(notes, filters);
 });
 
 document.querySelector("#sort").addEventListener("change", e => {
-    console.log(e.target.value);
+  console.log(e.target.value);
 });
 
-document.querySelector('#removeAll').addEventListener("click", e => {
-    removeAll();
+document.querySelector("#removeAll").addEventListener("click", e => {
+  removeAll();
 });
--- notes-functions.js
+++ notes-functions.js
@@ -1,3 +1,5 @@
+// getting saved nots from localStorage
+// Returning the the parsed JSON content or an empty Array
 const getSavedNotes = () => {
     const noteJSON = localStorage.getItem('notes');
     if (noteJSON !== null) {
@@ -7,15 +9,18 @@ const getSavedNotes = () => {
     }
 }
 
+Saving notes
 const saveNotes = (notes) => {
     localStorage.setItem('notes', JSON.stringify(notes));
 }
 
+// Removing notes using the ID, and calling whithin the generateDom function
 const removeNote = id => {
     const fnote = notes.findIndex(note => note.id === id);
     if (fnote > -1) notes.splice(fnote, 1);
 }
 
+// Crete the DOM element and appending to the UI
 const generateNoteDom = (note) => {
     const divItem = document.createElement('div');
     const pTag = document.createElement('a');
@@ -24,11 +29,13 @@ const generateNoteDom = (note) => {
     const icon = document.createElement('i');
     const btnDel = document.createElement('span');
 
+    // Event Listener removing note
     btnDel.addEventListener('click', (id) => {
         removeNote(note.id);
         saveNotes(notes);
         renderNotes(notes, filters);
     });
+
 
     const iconDel = document.createElement('i');
     btnDel.classList.add('del', 'd-inline-block');
@@ -39,6 +46,8 @@ const generateNoteDom = (note) => {
 
     icon.classList.add("fas", "fa-calendar-check");
     console.log(note);
+
+    // verifying if the element is empty or note, diferente contente is rendered
     if (note.title.length > 0 || note.body.length > 0) {
         pTag.textContent = note.title;
         pTag.setAttribute("href", `./edit.html#${note.id}`);
@@ -48,6 +57,7 @@ const generateNoteDom = (note) => {
         divTxtBody.textContent = '___';
     };
 
+    // If both elements are filled out ?  Add on icon
     !!note.title && !!note.body ? divTxtBody.appendChild(icon) : false;
     pTag.appendChild(divTxtBody);
     divItem.appendChild(pTag);
@@ -55,17 +65,21 @@ const generateNoteDom = (note) => {
     return divItem;
 }
 
+/* Loop through the Object veryfying if the element in the Filters obj
+exist whithin notes Objs  */
 const renderNotes = (notes, filters) => {
     // debugger
     let filteredNotes = notes.filter(note => {
         return note.title.toLowerCase().includes(filters.serachText.toLowerCase());
     });
     document.querySelector('#notes').innerHTML = '';
+/* 
+    Loop trogh the let that contains all Notes
+    Creates an constant bind the generateNoteDom 
+    function passing the note(forEach) as argument */
     filteredNotes.forEach(note => {
         const generatedDom = generateNoteDom(note);
         document.querySelector('#notes').appendChild(generatedDom);
-
-
     });
 
 }
